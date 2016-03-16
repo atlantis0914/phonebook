5a23a258eb01cd5c3b422976816833e565f018db
atlantis0914 <atlantis0914@hotmail.com>
2016-03-17 01:52:35 +0800

    Implement the append and findName.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -5,11 +5,35 @@
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastname[], entry *pHead)
 {
-    /* TODO: implement */
+    while (pHead != NULL) {
+        if(strcasecmp(lastname, pHead->lastNameList[pHead->numElements-1]) <= 0) {
+            unsigned int i;
+            for(i = 0; i < pHead->numElements; i++) {
+                if (strcasecmp(lastname, pHead->lastNameList[i]) == 0) {
+                    strcpy(pHead->lastName, lastname);
+                    return pHead;
+                }
+            }
+        }
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    if(e->numElements < MAX_NUM_OF_NAME) {
+        strcpy(e->lastNameList[e->numElements], lastName);
+    } else {
+        // printf("%s\r\n", e->lastNameList[e->numElements-1]);
+        e->pNext = (entry *) malloc(sizeof(entry));
+        e = e->pNext;
+        e->numElements = 0;
+        strcpy(e->lastNameList[e->numElements], lastName);
+        e->pNext = NULL;
+    }
+    e->numElements++;
+    return e;
 }
+
+
