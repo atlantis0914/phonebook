d45945d1797cd44b4bb43ddaf9a49764dcb9a6c0
atlantis0914 <atlantis0914@hotmail.com>
2016-03-17 01:42:24 +0800

    Rewrite the entry structure.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,10 +2,12 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
-
+#define MAX_NUM_OF_NAME    100
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
+
+/*
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
@@ -19,6 +21,16 @@ typedef struct __PHONE_BOOK_ENTRY {
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
+*/
+
+typedef struct __PHONE_BOOK_ENTRY {
+    unsigned int numElements;
+    char lastName[MAX_LAST_NAME_SIZE];
+    char lastNameList[MAX_NUM_OF_NAME][MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_ENTRY *pNext;
+
+} entry;
+
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
