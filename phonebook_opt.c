489e8b35a311d7c921949058d73fc0d5ee673291
atlantis0914 <atlantis0914@hotmail.com>
2016-04-29 01:14:28 +0800

    Implement the find and append method of b-tree

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,104 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastname[], entry *pHead)
+
+char *findName(char lastname[], bTreeNode *node, int ht)
 {
-    /* TODO: implement */
+    bTreeEntry *children = node->children;
+    int j;
+    if(ht == 0){
+        for(j = 0; j < node->degree; j++){
+            if(strcmp(lastname, children[j].lastName) == 0){
+                return lastname;
+            }
+        }
+    }
+    else{
+        for(j = 0; j < node->degree; j++){
+            if(strcmp(lastname, children[j].lastName) == 0)
+                return lastname;
+            else if(j+1 == node->degree || strcmp(lastname, children[j+1].lastName) < 0)
+                return findName(lastname, children[j].pNext, ht-1);
+        }
+     }
     return NULL;
 }
 
-entry *append(char lastName[], entry *e)
+
+bTreeNode *split(bTreeNode *node)
 {
-    return NULL;
+    int j;
+    bTreeNode *e = (bTreeNode *)malloc(sizeof(bTreeNode));
+    e->degree = DEGREE >> 1;
+    node->degree = DEGREE >> 1;
+    for(j = 0; j < DEGREE >> 1; j++){
+        e->children[j] = node->children[(DEGREE>>1) + j];
+    }
+    return e;
 }
+
+bTreeNode *appendHelp(char lastName[], bTreeNode *node, int ht)
+{
+    int i, j;
+    bTreeEntry *e = (bTreeEntry *)malloc(sizeof(bTreeEntry));
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    if(ht == 0) {
+        for(j = 0; j < node->degree; j++) {
+            if(strcmp(lastName, node->children[j].lastName) < 0)
+                break;
+        }    
+    }
+
+    else {
+        for(j = 0; j < node->degree; j++) {
+            if((j+1 == node->degree) || strcmp(lastName, node->children[j+1].lastName) < 0) {
+               bTreeNode *u = appendHelp(lastName, node->children[j++].pNext, ht-1);
+               if(u == NULL)
+                   return NULL;
+               strcpy(e->lastName, u->children[0].lastName);
+               e->pNext = u;
+               break; 
+            }
+        }
+    }
+    
+    for(i = node->degree; i > j; i--) {
+        node->children[i] = node->children[i-1];
+    }
+    node->children[j] = *e;
+    node->degree++;
+    if(node->degree < DEGREE){
+        return NULL;
+    }
+    else{
+        return split(node);
+    }
+}
+
+bTreeNode *append(char lastName[], bTreeNode *root, int *ht)
+{
+    bTreeNode *e = appendHelp(lastName, root, *ht);
+    if(e == NULL)
+        return root;
+    bTreeNode *u = (bTreeNode *)malloc(sizeof(bTreeNode));
+    u->degree = 2;
+    strcpy(u->children[0].lastName, root->children[0].lastName);
+    u->children[0].pNext = root;
+    strcpy(u->children[1].lastName, e->children[0].lastName);
+    u->children[1].pNext = e;
+    root = u;
+    *ht = *ht + 1;
+    return root;      
+}
+
+
+
+
+
+
+
+
